71d040b437d6c81faf99a9de5c5a767b6280f2aa
Alexandre Costa <alecosta19@gmail.com>
2020-08-19 22:01:01 -0300

    Getting user name and add. some variables

--- main.sh
+++ main.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-#First Lines ...
-printf "Hello World, I'm $USER\n"
+get_user_name() {
+#Get the new user's name
+	val=$(whiptail --title "WHIPTUSER" --inputbox "USER NAME: " 10 60 3>&1 1>&2 2>&3)
+	echo $val
+}
+
+usr_login='zzz'
+usr_id=-1
+group_id=-2
+usr_name=$(get_user_name)
+usr_shell="/bin/bash"
+if [ -z "$usr_name" ]
+then
+	clear	
+	printf "... TYPE A NAME TO THE NEW USER ... \n"
+	exit 65
+fi
+
+usr_home="/home/$usr_name"
+
+printf "$usr_login:x:$usr_id:$group_id:$usr_name,,,:$usr_home:$usr_shell\n"
+
